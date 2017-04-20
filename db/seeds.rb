2f39d68e3196eda780196c371caf1e84a849a99a
Peter Cheng <petercheng@Peters-Air.home>
2017-04-20 00:01:43 -0400

    remove bad seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,3 @@ Photo.create(
   title: 'Lovely Corgi',
   owner_id: 1
 )
-
-Photo.create(
-  photo_url: 'http://buddysdaycare.com/wp-content/uploads/2014/10/ServiceHP1.jpg',
-  title: 'Amazing Dog',
-  owner_id: 3
-)
-
-Pet.create(
-  pet_name: "billy",
-  user_id: 3
-)
