6b65cfef5b891ea4984bc6befc6928a9a6be1a16
Peter Cheng <petercheng@Peters-Air.home>
2017-04-20 00:18:02 -0400

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ User.create(
   password_digest: BCrypt::Password.create("password")
 )
 
+Photo.delete_all
+
 Photo.create(
   photo_url: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/09/chompers151.jpg',
   title: 'Lovely Corgi',
