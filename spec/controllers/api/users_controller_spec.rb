d08bd85f7fb6953454c2da7c3ffad88455eeb80b
Peter Cheng <rhoxwarrior@gmail.com>
2017-07-02 11:37:19 -0400

    edited readme to contain live site

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,10 +1,21 @@
+require 'rails_helper'
+
 RSpec.describe Api::UsersController, type: :controller do
-  describe "post #create" do
+  let(:user) { User.create!(username: "rickandmorty", password: "season3") }
+  describe "POST #create" do
     context "with valid params" do
       it "renders user on success" do
-        post :create, user: { username: "rickandmorty", password: "season3" }
+        post :create, format: :json, user: { username: "harrypotter", password: "isthebest" }
+        # post '/api/users', {format: :json}, user: { username: "harrypotter", password: "isthebest" }
         expect(response).to render_template("api/users/show")
       end
+    end
+  end
+
+  describe "GET #show" do
+    it "renders show template" do
+      get :show, id: user.id
+      expect(response).to render_template("api/users")
     end
   end
 end
