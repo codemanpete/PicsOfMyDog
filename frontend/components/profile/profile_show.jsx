33bf8647f66d3332702d3e2e503a514ff9de2bbb
Peter Cheng <rhoxwarrior@gmail.com>
2017-07-26 17:37:10 -0400

    Updated contact information for welcome page.

--- frontend/components/profile/profile_show.jsx
+++ frontend/components/profile/profile_show.jsx
@@ -14,6 +14,7 @@ const customStyle = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
+    textAlign: 'center'
   }
 };
 
--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -9,6 +9,16 @@ const Welcome = () => {
         <h2>Showcase your adventures with your favorite companion.</h2>
         <Link to="/signup" className="intro-button">Get Started</Link>
       </div>
+      <div className="about-me">
+        <p>500Picsofmydog is a 'pet' project by Peter Cheng to showcase his web development skills.</p>
+        <p>He used Rails for the backend and React/Redux for the frontend.</p>
+        <p>Follow these links to get in touch with Peter, find out more about what he does and hire him.</p>
+        <a href="https://www.linkedin.com/in/peter-cheng-392266131/">LinkedIn</a>
+        <br />
+        <a href="https://github.com/shadypeet">Github</a>
+        <br />
+        <a href="mailto:peter.yc.cheng@gmail.com">Gmail</a>
+      </div>
     </div>
   );
 };
